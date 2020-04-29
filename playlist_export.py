9687a9a3f6132e8fb14e3441e4cc13ea7f300ca7
Ignacio Marín Brañas <ignaciomarinbranas@Ignacios-MBP.fritz.box>
2020-04-29 21:54:49 +0200

    merged export and parsing in one script

--- playlist_export.py
+++ playlist_export.py
@@ -1,11 +1,10 @@
 """
 SCRIPT TO EXPORT ALL PLAYLISTS OF A SPOTIFY USER
-TO A SERIES OF PAGINATED JSON FILES
-WITH A DEFAULT OF 100 SONGS A PIECE (API request max limit)
+TO CSV FILES
 """
 from spotify_functions import *
 
-print("\nThis script will export all playlists of a Spotify user account into paginated JSON files.")
+print("\nThis script will export all playlists of a Spotify user account into .csv files.")
 print("It assumes you have already generated the necessary credentials and stored them in './tokens.json'")
 print("If you have not, use './get_credentials.py' to generate a new set of credentials")
 
@@ -17,8 +16,12 @@ if credential_check(desired_scope="playlist-read-private", credentials_dict=spot
     access_token = spotify_credentials["access_token"]
     refresh_token = spotify_credentials["refresh_token"]
 
+    user_details = get_spotify_profile_details(access_token)
+
+    user_id = user_details["id"]
+
     # get list of playlists
-    print("\nRetrieving list of playlists created and followed by the user")
+    print(f"\nRetrieving list of playlists created and followed by the user {user_id}")
 
     successful, playlists_df = get_all_playlists_df(access_token=access_token)
 
@@ -39,19 +42,26 @@ if credential_check(desired_scope="playlist-read-private", credentials_dict=spot
                                                                playlist_id=row['id'],
                                                                access_token=access_token,
                                                                playlist_name=row['name'],
-                                                               output_folder="playlists")
+                                                               output_folder="temp")
         pages_list.append(number_of_pages)
 
     playlists_df["pages"] = pd.Series(data=pages_list, index=playlists_df.index)
 
-    try:
-        playlists_df.to_csv("playlists/playlists.csv")
-        print("\nPlaylist summary df exported to 'playlists/playlists.csv'")
+    # parsing of json files into csv files
+    for index, row in playlists_df.iterrows():
+        playlist_name = row['name']
+        playlist_pages = row["pages"]
 
-    except:
-        print("\nError - Playlist summary df could not be exported")
+        playlist_df = playlist_jsons_to_csv(playlist_name=playlist_name,
+                                            playlist_pages=playlist_pages,
+                                            jsons_folder="temp")
 
-    print("\nSuccess - Export of user playlists completed")
+        export_playlist_csv(playlist_name=playlist_name,
+                            playlist_df=playlist_df,
+                            output_folder=user_id)
+
+    print(f"\nSuccess - Export of user {user_id} playlists completed")
+    print(f"\nOutput folder: {user_id}")
 
 else:
     print("\nError - Credentials are not valid")
